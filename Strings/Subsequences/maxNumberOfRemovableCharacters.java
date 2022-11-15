28169230ea6d3779482d09f27e587e927ec078a7
Kshitij-1373 <80763029+Kshitij-1373@users.noreply.github.com>
2022-11-15 23:13:18 +0530

    Create maxNumberOfRemovableCharacters.java

---
+++ Strings/Subsequences/maxNumberOfRemovableCharacters.java
@@ -0,0 +1,44 @@
+//[problem link](https://leetcode.com/problems/maximum-number-of-removable-characters/description/) 
+class Solution {
+    public int maximumRemovals(String s, String p, int[] r) {
+        char[] str = s.toCharArray();
+        char[] ptr = p.toCharArray();
+        
+        int start = 0;
+        int end =r.length -1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            for(int i=start; i<= mid; i++){
+                //acts as the removal 
+                str[r[i]] = '*'; 
+            }
+            if(isSubsequence(str, ptr)){
+                start = mid+1;
+            }
+            else{
+                end = mid -1;
+                for(int i=start; i<=mid; i++){
+                    // now we again unmark the previously removed indices..
+                    // as now we are searching on latter half
+                    
+                    str[r[i]] = s.charAt(r[i]); 
+                }
+            }
+        }
+        return start;
+    }
+    
+    private boolean isSubsequence(char[] s, char[] p){
+        int i =0;
+        int j =0;
+        
+        while(i <s.length && j < p.length){
+            if(s[i] == p[j]){
+                j++;
+            }
+            i++;
+        }
+        return j == p.length;
+    }
+}
