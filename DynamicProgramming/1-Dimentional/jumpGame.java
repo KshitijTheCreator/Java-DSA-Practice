4e9bd00dc4b0867e92813aacc5317fa3e5583055
Kshitij-1373 <80763029+Kshitij-1373@users.noreply.github.com>
2022-11-16 23:46:40 +0530

    Create jumpGame.java

---
+++ DynamicProgramming/1-Dimentional/jumpGame.java
@@ -0,0 +1,28 @@
+//[problemLink](https://leetcode.com/problems/count-ways-to-build-good-strings/submissions/842674738/)
+class Solution {
+    Integer[] dp;
+    int MOD = 1000000007;
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        dp = new Integer[high +1];
+        int ans =0;
+        for(int i=low; i<= high; i++){
+            ans =(ans + helper(i, zero, one)) % MOD;
+        }
+        return ans;
+    }
+    private int helper(int n, int zero, int one){
+        if(n <0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        if(dp[n] != null){
+            return dp[n];
+        }
+        int takeZero = helper(n-zero, zero, one) % MOD;
+        int takeOne = helper(n-one, zero, one) % MOD;
+
+        return dp[n] = (takeZero + takeOne) % MOD;
+    }
+}
